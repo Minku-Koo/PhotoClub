2f1d822aa8b95fb184a7e2dad78f82de8f2fadb9
구민구 <corleone@kakao.com>
2021-09-23 11:43:49 +0900

    Create app.py

---
+++ www/app.py
@@ -0,0 +1,13 @@
+﻿# flask create app
+# Project Start : 20210923
+
+from flask import Flask
+from .views import views
+import os
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(views)
+    app.secret_key = "this1sCjUUP0t0C1bu"
+    
+    return app
