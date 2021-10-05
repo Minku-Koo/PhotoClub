ecc0dc623a19af6d6251895f08ff4e96de538c8d
구민구 <corleone@kakao.com>
2021-10-06 00:01:51 +0900

    Update views.py

--- www/views.py
+++ www/views.py
@@ -16,12 +16,19 @@ __db__ = pymysql.connect(
 @views.route("/", methods=["GET"])
 def index():
     print("CJU Photo Club Flask Server")
+    session["admin"] = False
     return render_template("index.html")
 
 @views.route("/manager", methods=["GET"])
 def manager():
     print("Manager Page")
-    return render_template("manager.html")
+    if session["admin"] :
+        print("session[admin]", session["admin"])
+        return render_template("manager.html")
+    else:
+        msg = "올바르지 못한 접근입니다."        
+        print("올바르지 못한 접근입니다.")
+        return render_template("login.html", msg=msg)
 
 @views.route("/login", methods=["GET"])
 def login():
@@ -35,22 +42,61 @@ def check():
     manager_pw = info.manager_password
     # login success
     if input_pw == manager_pw:
+        session["admin"] = True
+        print(session["admin"])
         return render_template("manager.html")
-    # login failed
-    else:
+    elif input_pw != manager_pw :
         msg = "비밀번호 틀렸습니다.\n비밀번호는 회장에게 물어보세요."        
+        print("비번 틀렷어요")
         return render_template("login.html", msg=msg)
+    # login failed
+    elif not session["admin"] :
+        msg = "올바르지 못한 접근입니다."        
+        print("올바르지 못한 접근입니다.")
+        return render_template("login.html", msg=msg)
+
+    return render_template("login.html", msg="")
 
 @views.route("/intro", methods=["GET"])
 def intro():
     print("Intro Page")
     sql = Sql(__db__)
-    poster, chairman_num, intro_text = sql.get_site()
+    poster, intro_text = sql.get_site()
     
-    name = sql.get_username(chairman_num)
-    profile = sql.get_userpic(chairman_num)
+    #추후에 user에서 회장 찾아서 입력
+    name = sql.get_username(1)
+    profile = sql.get_userpic(1)
 
     return render_template("intro.html",
                             profile=profile, 
                             chairman=" ".join(name), 
                             intro_text=intro_text)
+
+
+@views.route("/setuser", methods=["GET"])
+def setuser():
+    print("setuser page")
+    if not session["admin"] :
+        msg = "올바르지 못한 접근입니다."    
+        print("올바르지 못한 접근입니다.")
+        return render_template("login.html", msg=msg)
+    return render_template("set_user.html")
+
+@views.route("/setsite", methods=["GET"])
+def setsite():
+    print("setsite page")
+    if not session["admin"] :
+        msg = "올바르지 못한 접근입니다."    
+        print("올바르지 못한 접근입니다.")
+        return render_template("login.html", msg=msg)
+    
+    sql = Sql(__db__)
+    history = sql.get_all("history")
+    events =  sql.get_all("club_event")
+    faq_list = sql.get_all("faq")
+    return render_template("set_site.html", 
+                            history=history,
+                            events=events,
+                            faq_list = faq_list)
+
+
